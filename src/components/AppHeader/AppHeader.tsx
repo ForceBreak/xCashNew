7b3ac926b2c2c4bb12497ad6a6d1f3be6daed512
ForceBreak <gbuecjd18@gmail.com>
2026-08-31 19:58:01 +0200

    minor update for header

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -8,7 +8,7 @@ import { useAuth } from '@/providers/auth-provider';
 import { COINS_PER_DOLLAR } from '@/constants';
 
 export default function AppHeader() {
-  const { user } = useAuth();
+  const { user, isLoading } = useAuth();
   const calculateUSD = (balance_coins: number) =>
     (balance_coins / COINS_PER_DOLLAR).toFixed(2);
 
@@ -18,7 +18,12 @@ export default function AppHeader() {
         <AppLogo />
       </div>
       <div className="flex gap-5 items-center ml-auto">
-        {user ? (
+        {isLoading ? (
+          <div className="flex items-center gap-3 h-10 px-4 rounded-lg font-base-bold leading-none bg-gray-750 animate-pulse">
+            <div className="h-6 w-6 bg-gray-700 rounded animate-pulse"></div>
+            <div className="h-6 w-6 bg-gray-700 rounded animate-pulse"></div>
+          </div>
+        ) : user ? (
           <>
             <span className="hidden md:flex items-center gap-3 h-10 px-4 rounded-lg font-base-bold leading-none bg-gray-750">
               <Banknote size={24} className="text-yellow-400" />
