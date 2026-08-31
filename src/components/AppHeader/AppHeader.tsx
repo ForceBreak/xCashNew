3529999f6952a9f2b3cdf4ef2c27c099710513ae
ForceBreak <gbuecjd18@gmail.com>
2026-08-31 19:41:42 +0200

    update header with actual coins balance

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -5,9 +5,12 @@ import { Banknote, Coins } from 'lucide-react';
 import UserMenu from '@/components/AppHeader/UserMenu';
 import ModalSignIn from '@/components/Modals/SignIn';
 import { useProfile } from '@/hooks/use-profile';
+import { COINS_PER_DOLLAR } from '@/constants';
 
 export default function AppHeader() {
   const { data: user, isLoading } = useProfile();
+  const calculateUSD = (balance_coins: number) =>
+    (balance_coins / COINS_PER_DOLLAR).toFixed(2);
 
   return (
     <div className="w-full h-16 flex items-center justify-between px-4 border-b border-secondary sticky top-0 left-0 z-10 bg-gray-900">
@@ -24,11 +27,11 @@ export default function AppHeader() {
           <>
             <span className="hidden md:flex items-center gap-3 h-10 px-4 rounded-lg font-base-bold leading-none bg-gray-750">
               <Banknote size={24} className="text-yellow-400" />
-              <span>$0</span>
+              <span>${calculateUSD(user.balance_coins)}</span>
             </span>
             <span className="flex items-center gap-3 h-10 px-4 rounded-lg font-base-bold leading-none bg-gray-750">
               <Coins size={20} className="text-yellow-400" />
-              <span>0</span>
+              <span>{user.balance_coins}</span>
             </span>
 
             <UserMenu />
---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const COINS_PER_DOLLAR: number = Number(
+  process.env.NEXT_PUBLIC_COINS_PER_DOLLAR,
+);
